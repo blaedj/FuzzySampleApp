035db0c8376f5f1201434f092e347bacad140e49
Blaed <blaedj@gmail.com>
2013-01-05 09:28:16 -0600

    TDD for home n help pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,7 +6,14 @@ describe "Static pages" do
 
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
-      page.should have_conetent('Sample App')
+      page.should have_content('Sample App')
     end
   end
-end
+
+  describe "Help page" do
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      page.should have_content('Help')
+      end
+    end
+end #end describe Static pages
