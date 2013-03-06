f58f36b388f0107bd535f835e0429a0561314f37
Blaed <blaedj@gmail.com>
2013-03-06 07:06:45 -0600

    finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,24 +1,37 @@
 class UsersController < ApplicationController
 
-def index
-    @users  = User.all
-end
-
-#GET /users/find_all_b
-def find_all_b
-    @users = User.where(:name => 'bobby')
+    def index
+        @users  = User.all
     end
 
-#GET /users/find_all_n
-def find_all_n
-	@users = User.where(:name => 'noah')
-end
+    #GET /users/find_all_b
+    def find_all_b
+        @users = User.where(:name => 'bobby')
+        end
 
-def show
-    @user = User.find(params[:id])
-end
+    #GET /users/find_all_n
+    def find_all_n
+    	@users = User.where(:name => 'noah')
+    end
 
-def new
-end
+    def show
+        @user = User.find(params[:id])
+    end
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.new( params[:user] )
+
+        if @user.save
+            flash[:success] = "Welcome to our website!"
+            redirect_to @user
+        else
+            render 'new'
+        end
+
+    end
 
 end
