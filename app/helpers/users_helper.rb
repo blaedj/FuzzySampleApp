f8aa7f05de9cfefa956889068cf4403fa7bb3364
np <noah@thesubtlety.com>
2013-04-20 13:53:58 -0400

    fixing stupid conflicts

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,18 +1,11 @@
 module UsersHelper
 
-<<<<<<< HEAD
+
 def gravatar_for(user, options = {size: 50})
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     size = options[:size]
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
-  end
-=======
-    def gravatar_for(user)
-        gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-        gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
-        image_tag(gravatar_url, alt: user.name, class: "gravatar")
-    end
+end
 
->>>>>>> 3ea604a7550bbaca10ffc68b138242921a9c623a
 end
